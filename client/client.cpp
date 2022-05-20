ac8bdb70af353b48b74d36c3d2da6156d065c957
kdokm <897194064@qq.com>
2022-05-20 12:08:44 +0800

    switch client to windows and add .gitignore

--- client/client.cpp
+++ client/client.cpp
@@ -1,22 +1,22 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "sock.h"
 
 const int size = 4096;
 
-void send_packed_str(int sock, unsigned char* buf, char *init, const unsigned int len) {
+void send_packed_str(SOCKET sock, unsigned char* buf, char *init, const unsigned int len) {
 	unsigned char lsb = len & 0xff;
     unsigned char msb = len >> 8;
     buf[0] = msb;
     buf[1] = lsb;
     memcpy(buf+2, reinterpret_cast<unsigned char*>(init), len);
-    send(sock, buf, len+2, 0);
+    int ret = send(sock, reinterpret_cast<char*>(buf), len+2, 0);
+	if (SOCKET_ERROR == ret) {
+		printf("send fails: %d", WSAGetLastError());
+		closesocket(sock);
+		exit(1);
+	}
 	memset(buf, 0, len+2);
 }
 
@@ -24,19 +24,52 @@ int main(int argc, char *argv[])
 {
 
 	//tcp socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in saddr;
-    memset(&saddr, 0, sizeof(saddr));
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(8888);
-    saddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    if (connect(sock, (struct sockaddr*)&saddr, sizeof(saddr)) < 0)
-    {
-        printf("connect fails\n");
+    WSADATA wsaData;
+	int ret = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (0 != ret)
+	{
+		printf("init fails\n");
         exit(1);
-    }
+	}
+
+    struct addrinfo *result = NULL;
+	struct addrinfo hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+    char ip[100];
+    strcpy(ip, "127.0.0.1");
+	char port[100];
+	strcpy(port, "8888");
+
+	ret = getaddrinfo(ip, port, &hints, &result);
+	if (0 != ret)
+	{
+		printf("getaddr fails\n");
+        exit(1);
+	}
+
+    SOCKET sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (INVALID_SOCKET == sock)
+	{
+        freeaddrinfo(result);
+		printf("socket fails\n");
+        exit(1);
+	}
+
+    ret = connect(sock, result->ai_addr, (int)result->ai_addrlen);
+	if (SOCKET_ERROR == ret)
+	{
+		freeaddrinfo(result);
+		closesocket(sock);
+		printf("socket fails\n");
+        exit(1);
+	}
+
+    freeaddrinfo(result);
 
     unsigned char buf[size];
     memset(buf, 0, sizeof(buf));
@@ -44,16 +77,16 @@ int main(int argc, char *argv[])
 	memset(msg, 0, sizeof(msg));
 
     char init[size];
-    memset(init, 0, sizeof(init));
     char temp[size];
+    system("cls");
     printf("Welcome to the game! Please enter V to verify your account or enter C to create a new account: ");
     scanf("%s", temp);
     while (strlen(temp) != 1 || (strncmp(temp, "V", 1) != 0 && strncmp(temp, "C", 1) != 0)) {
         printf("Invalid option, please enter again.\n");
         scanf("%s", temp);
     }
-    strcat(init, temp);
-    printf("Please enter your id: ");
+    strcpy(init, temp);
+    printf("\nPlease enter your id: ");
     scanf("%s", temp);
     strcat(init, temp);
     printf("Please enter your password: ");
@@ -64,11 +97,7 @@ int main(int argc, char *argv[])
     send_packed_str(sock, buf, init, len);
     while (true)
     {
-		/*sprintf(buf, "%c%cCkkk\npw", ++count);
-		printf("sending: %s", buf);
-		send(sock, buf, strlen(buf), 0);
-		memset(buf, 0, sizeof(buf));
-
+		/*
 		while (true) {
 			recv(sock, buf, sizeof(buf), 0);
 			strcat(msg, buf);
@@ -82,12 +111,8 @@ int main(int argc, char *argv[])
         printf("received: %s\n", msg);
         memset(msg, 0, sizeof(msg));
 
-		if (count == 10) {
-			sprintf(buf, "exit\n");
-			send(sock, buf, strlen(buf), 0);
-			break;
-		}*/
+		*/
     }
-	close(sock);
+	closesocket(sock);
     return 0;
 }
