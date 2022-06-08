b86aa68bad706d2e1a5502812c88504700a8bc07
kdokm <897194064@qq.com>
2022-06-08 08:59:15 +0800

    add a basic monster ai

--- server/service/monster.lua
+++ server/service/monster.lua
@@ -1,42 +1,57 @@
 local skynet = require "skynet"
 require "skynet.manager"
+local utils = require "utils"
 
 local CMD = {}
 local minDist = 3
+local pos = {}
+local dir = utils.getInitDir()
 local target = {}
 
 local function dist(x1, y1, x2, y2)
 	return math.abs(x1-x2) + math.abs(y1-y2)
 end
 
-local function inRange(x1, y1, x2, y2, r)
-	local d = x1 - x2
-	if d > r or d < -r then
-		return false
+local function moveDir(current, target)
+	if current > target then
+		return -1
+	else
+		return 1
 	end
-	d = y1 - y2
-	if d > r or d < -r then
-		return false
-	end
-	return true
 end
 
 local function action()
-
+	local x, y = utils.decodeDir(dir)
+	if utils.inRangeSquare(pos.x+x, pos.y+y, target.x, target.y, 1) then
+		skynet.call("scene", "lua", "attack", "wolf")
+	else
+		local nextDir
+		if math.abs(pos.x-target.x) > math.abs(pos.y-target.y) then
+			nextDir = utils.encodeDir(moveDir(pos.x, target.x), 0)
+		else
+			nextDir = utils.encodeDir(0, moveDir(pos.y, target.y))
+		end
+		skynet.call("scene", "lua", "move", "wolf", nextDir)
+		dir = nextDir
+	end
 end
 
 function CMD.react(attr)
-	target.dist == nil
+	target.dist = nil
 	for k, v in pairs(attr.updates) do
-		local d = dist(attr.x, attr.y, v.x, v.y)
-		if d <= minDist then
-			if target.dist == nil or d < target.dist then
-				target.x = v.x
-				target.y = v.y
-				target.dist = d
+		if v.id ~= "wolf" then
+			local d = dist(attr.x, attr.y, v.x, v.y)
+			if d <= minDist then
+				if target.dist == nil or d < target.dist then
+					target.x = v.x
+					target.y = v.y
+					target.dist = d
+				end
 			end
 		end
 	end
+	pos.x =attr.x
+	pos.y = attr.y
 end
 
 skynet.start(function()
@@ -52,7 +67,7 @@ skynet.start(function()
 			if target.dist ~= nil then
 				action()
 			end
-			skynet.sleep(50)
+			skynet.sleep(100)
 		end
 	end)
 end)
